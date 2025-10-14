10fd16ef2c60451e463a515d9b20bfd95a8abfe9
Bolle-reddat <leo.bolle06@gmail.com>
2025-10-14 12:03:52 +0200

    Code serveur web v3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <WiFi.h>
 #include <WebServer.h>
-
 const char* ssid = "BTS_CIEL";
 const char* password = "ERIR1234";
 WebServer server(80);
@@ -14,38 +13,25 @@ page += "<html lang='fr'>";
 page += "<head>";
 page += " <title>Serveur ESP32</title>";
 
-page += " <meta name='viewport' content='width=device-width, initial-scale=1'>";
+page += " <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8' />";
 
-page += "<style>";
-page += " html {";
-page += " font-family: Helvetica;";
-page += " margin: 0px auto;";
-page += " text-align: center;";
-page += " }";
-page += " h1 {";
-page += " color: #0F3376;";
-page += " padding: 2vh;";
-page += " }";
-page += " .button {";
-page += " background-color: #4CAF50; /* Vert */";
-page += " border: none;";
-page += " border-radius: 6px; /* Angle arrondi */";
-page += " color: white;";
-page += " padding: 15px 32px;";
-page += " text-align: center;";
-page += " text-decoration: none;";
-page += " font-size: 30px;";
-page += " }";
-
-page += " .button2 {";
-page += " background-color: #f44336; /* Rouge */";
-page += " }";
-page += "</style>";
+page += " <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>";
 page += "</head>";
 page += "<body>";
-page += " <h1>Projet SNbot</h1>";
-page += " <p><a href='/on'><button class='button'>ON</button></a></p>";
-page += " <p><a href='/off'><button class='button button2'>OFF</button></a></p>";
+page += " <div class='w3-card w3-blue w3-padding-small w3-jumbo w3-center'>";
+page += " <p>ÉTAT LED: "; 
+page += texteEtatLed[etatLed];
++ "</p>";
+page += " </div>";
+page += " <div class='w3-bar'>";
+page += " <a href='/on' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>ON</a>";
+page += " <a href='/off' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>OFF</a>";
+page += " </div>";
+page += " <div class='w3-center w3-padding-16'>";
+page += " <p>Serveur hébergé sur un ESP32</p>";
+page += " <i>Projet SNBot</i>";
+page += " </div>";
+
 page += "</body>";
 page += "</html>";
 server.setContentLength(page.length());
@@ -74,8 +60,6 @@ void setup()
 Serial.begin(9600);
 delay(1000);
 Serial.println("\n");
-
-
 pinMode(led, OUTPUT);
 digitalWrite(led, LOW);
 WiFi.persistent(false);
@@ -86,6 +70,8 @@ while (WiFi.status() != WL_CONNECTED)
 Serial.print(".");
 delay(100);
 }
+
+
 Serial.println("\n");
 Serial.println("Connexion etablie!");
 Serial.print("Adresse IP: ");
