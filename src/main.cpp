f104c3aa43cd8e86ecbf225c13f9e93dff751925
Bolle-reddat <leo.bolle06@gmail.com>
2025-10-14 09:50:50 +0200

    Fréquence clignotement 1Hz

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ pinMode(led, OUTPUT); // Configure la broche GPIO16 en sortie
 void loop()
 {
 digitalWrite(led, HIGH);// Ecrit la valeur HIGH (=1) sur la broche GPIO16
-delay(1000); // Attendre 1000ms
+delay(500); // Attendre 1000ms
 digitalWrite(led, LOW); // Ecrit la valeur LOW (=0) sur la broche GPIO16
-delay(1000);
+delay(500);
 }
